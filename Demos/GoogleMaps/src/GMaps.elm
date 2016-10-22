04d9416450f57b18ba704db27882ab2f84593d94
Tyler Jennings <jenningstcj@gmail.com>
2016-10-22 19:48:05 -0400

    added Elm demo2

---
+++ Demos/GoogleMaps/src/GMaps.elm
@@ -0,0 +1,10 @@
+port module GMaps exposing (..)
+
+import SharedModels exposing (GMPos)
+
+
+-- PORTS
+
+port moveMap : GMPos -> Cmd msg
+
+port mapMoved : (GMPos -> msg) -> Sub msg
---
+++ Demos/GoogleMaps/src/Main.elm
@@ -0,0 +1,152 @@
+import Html exposing (Html, div, p, text, button)
+import Html.Events exposing (onClick)
+import Html.App
+import Time exposing (..)
+import Http
+import Json.Decode exposing (..)
+import Task
+
+
+import SharedModels exposing (GMPos)
+import GMaps exposing (moveMap, mapMoved)
+
+
+-- MAIN
+
+main : Program Never
+main = Html.App.program
+  { init = init
+  , view = view
+  , update = update
+  , subscriptions = subscriptions
+  }
+
+-- MODEL
+
+type alias Model =
+  { pos : GMPos
+  }
+
+type alias ISS_Lat_Long =
+  { latitude: Float
+  , longitude: Float
+  }
+
+type alias ISS_JSON =
+  { iss_position: ISS_Lat_Long
+  , message: String
+  , timestamp: Int
+  }
+
+-- UPDATE
+
+type Msg
+  = North
+  | South
+  | West
+  | East
+  | MapMoved GMPos
+  | FetchSucceed ISS_JSON
+  | FetchFail Http.Error
+  | FetchPosition Time
+
+update : Msg -> Model -> (Model, Cmd Msg)
+update msg model =
+  case msg of
+    North ->
+      let
+        newPos = GMPos (model.pos.lat + 1) model.pos.lng
+      in
+        ( { model | pos = newPos }
+        , moveMap newPos
+        )
+    South ->
+      let
+        newPos = GMPos (model.pos.lat - 1) model.pos.lng
+      in
+        ( { model | pos = newPos }
+        , moveMap newPos
+        )
+    West ->
+      let
+        newPos = GMPos model.pos.lat (model.pos.lng - 1)
+      in
+        ( { model | pos = newPos }
+        , moveMap newPos
+        )
+    East ->
+      let
+        newPos = GMPos model.pos.lat (model.pos.lng + 1)
+      in
+        ( { model | pos = newPos }
+        , moveMap newPos
+        )
+    MapMoved newPos ->
+      ( { model | pos = newPos }
+      , Cmd.none
+      )
+    FetchSucceed newISSPos ->
+      let
+        newPos = GMPos newISSPos.iss_position.latitude newISSPos.iss_position.longitude
+      in
+        ({model | pos = newPos }, Cmd.none)
+    FetchFail _ ->
+      (model, Cmd.none)
+    FetchPosition time ->
+      (model, getLocation)
+
+-- VIEW
+
+view : Model -> Html Msg
+view model =
+  div []
+    [ p [] [ text ("Latitude: " ++ toString model.pos.lat)]
+    , p [] [text ("Longitude: " ++ toString model.pos.lng)]
+    , button [ onClick North ] [ text "North" ]
+    , button [ onClick South ] [ text "South" ]
+    , button [ onClick West ] [ text "West" ]
+    , button [ onClick East ] [ text "East" ]
+    ]
+
+
+-- SUBSCRIPTIONS
+
+subscriptions : Model -> Sub Msg
+subscriptions model =
+  Sub.batch
+    [ mapMoved MapMoved
+    , Time.every (5 * second) FetchPosition
+    ]
+
+
+-- Http
+
+getLocation : Cmd Msg
+getLocation =
+  let
+    url = "http://api.open-notify.org/iss-now.json"
+  in
+    Task.perform FetchFail FetchSucceed (Http.get decodeISSPosition url)
+
+decodeLatLong : Decoder ISS_Lat_Long
+decodeLatLong =
+  object2 ISS_Lat_Long
+    ("latitude" := float)
+    ("longitude" := float)
+
+
+decodeISSPosition : Decoder ISS_JSON
+decodeISSPosition =
+  object3 ISS_JSON
+    ("iss_position" := decodeLatLong)
+    ("message" := string)
+    ("timestamp" := int)
+
+-- INIT
+
+init : (Model, Cmd Msg)
+init =
+  let
+    vienna = ( GMPos 48.2206636 16.3100206 )
+  in
+    ( Model vienna, moveMap vienna )
