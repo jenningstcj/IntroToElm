4abd3ef25901100f83c58f9f054db4287e09d091
Tyler Jennings <jenningstcj@gmail.com>
2016-10-27 14:02:56 -0400

    simplified Hello World demo

--- Demos/HelloWorld/Main.elm
+++ Demos/HelloWorld/Main.elm
@@ -8,31 +8,19 @@ import Html.Events exposing (onClick)
 
 main =
     Html.App.beginnerProgram
-        { model = { statement = "hello", size = 14 }
+        { model = { statement = "Hello World", size = 14 }
         , update = update
         , view = view
         }
 
 
-type Msg
-    = Increase
-    | Decrease
-
-
 update msg model =
     case msg of
-        Increase ->
-            { model | size = model.size + 2 }
-
-        Decrease ->
-            { model | size = model.size - 2 }
+        _ ->
+            model
+--}
 
 
 view model =
-    div [ style [ ( "fontSize", (toString model.size) ++ "pt" ) ] ]
-        [ text (model.statement ++ " world!")
-        , div []
-            [ button [ onClick Increase ] [ text "Increase" ]
-            , button [ onClick Decrease ] [ text "Decrease" ]
-            ]
-        ]
+    div []
+        [ span [ style [ ( "fontSize", (toString model.size) ++ "pt" ) ] ] [ text model.statement ] ]
